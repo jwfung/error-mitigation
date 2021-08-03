558dfbdbfcfef8eedbbd2c849df305616aaecdb3
jwfung <59128478+jwfung@users.noreply.github.com>
2021-08-03 15:59:51 -0400

    latin sqr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import Walkthrough from "./Walkthrough";
 import checkpointText from "./text/checkpointOne.js";
 import React from 'react';
 
-class App extends React.Component { 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      latinsqr: Math.floor( Math.random() * 5 ) + 1
+    }
+  }
 
   render () {
     return (
       <div className="App">
-        <Walkthrough sess={0} checkpointText={checkpointText}/>
+        <Walkthrough sess={0} checkpointText={checkpointText} latinsqr={this.state.latinsqr}/>
       </div>
     );
   }
