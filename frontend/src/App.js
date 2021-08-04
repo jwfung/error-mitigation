40454a67785221c3a3bf0f75ef44284e3f2ad733
jwfung <59128478+jwfung@users.noreply.github.com>
2021-08-03 23:33:38 -0400

    latinsqr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,14 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      latinsqr: Math.floor( Math.random() * 5 ) + 1
+      latinsqr: Math.floor( Math.random() * 5 ) 
     }
   }
 
   render () {
     return (
       <div className="App">
+        {console.log(this.state.latinsqr)}
         <Walkthrough sess={0} checkpointText={checkpointText} latinsqr={this.state.latinsqr}/>
       </div>
     );
