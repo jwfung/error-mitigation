39ce0758403ffe53fa248524388388ac50a90f9d
jwfung <59128478+jwfung@users.noreply.github.com>
2021-08-03 23:33:26 -0400

    adjusted questions

--- frontend/src/Questionaire.js
+++ frontend/src/Questionaire.js
@@ -13,6 +13,14 @@ class Questionaire extends React.Component {
       q5: '',
       q6: '',
       q7: '',
+      q8: '',
+      q9: '',
+      q10: '',
+      q11: '',
+      q12: '',
+      q13: '',
+      q14: '',
+      q15: '',
       complete: false,
       required: false
     }
@@ -47,7 +55,13 @@ class Questionaire extends React.Component {
           {q6: this.state.q6},
           {q7: this.state.q7},
           {q8: this.state.q8},
-
+          {q9: this.state.q9},
+          {q10: this.state.q10},
+          {q11: this.state.q11},
+          {q12: this.state.q12},
+          {q13: this.state.q13},
+          {q14: this.state.q14},
+          {q15: this.state.q15}
         ]
       }
       const { data } = this.props
@@ -64,7 +78,15 @@ class Questionaire extends React.Component {
               q4: '',
               q5: '',
               q6: '',
-              q7: ''
+              q7: '',
+              q8: '',
+              q9: '',
+              q10: '',
+              q11: '',
+              q12: '',
+              q13: '',
+              q14: '',
+              q15: ''
             })
             this.props.clearData();
             this.props.completeQuest();
@@ -102,7 +124,7 @@ class Questionaire extends React.Component {
           </div>
 
           <div className="wrapper">
-            <p class="statement">I would prefer using the AI assistant over having to shop myself.</p>
+            <p class="statement">The AI assistant was dependable.</p>
             <div>
               strongly disagree<input type="radio" Value="1" name="q2" onChange={this.onChange.bind(this)}/>
               <input type="radio" Value="2" name="q2" onChange={this.onChange.bind(this)}/>
@@ -114,7 +136,7 @@ class Questionaire extends React.Component {
           </div>
 
           <div className="wrapper">
-              <p class="statement">I trusted the AI assistant to correct its mistakes.</p>
+              <p class="statement">The AI assistant was responsive.</p>
             <div>
               strongly disagree<input type="radio" Value="1" name="q3" onChange={this.onChange.bind(this)}/>
               <input type="radio" Value="2" name="q3" onChange={this.onChange.bind(this)}/>
@@ -126,7 +148,7 @@ class Questionaire extends React.Component {
           </div>
 
           <div className="wrapper">
-            <p class="statement">I am satisfied with AI assistant's performance</p>
+            <p class="statement">The AI assistant made mistakes.</p>
             <div>
               strongly disagree<input type="radio" Value="1" name="q4" onChange={this.onChange.bind(this)}/>
               <input type="radio" Value="2" name="q4" onChange={this.onChange.bind(this)}/>
@@ -138,7 +160,7 @@ class Questionaire extends React.Component {
           </div>
 
           <div className="wrapper">
-            <p class="statement">Even though AI is not always accurate, I trusted AI to be efficient</p>
+            <p class="statement">I am happy with how the error was handled.</p>
             <div>
               strongly disagree<input type="radio" Value="1" name="q5" onChange={this.onChange.bind(this)}/>
               <input type="radio" Value="2" name="q5" onChange={this.onChange.bind(this)}/>
@@ -150,7 +172,7 @@ class Questionaire extends React.Component {
           </div>
 
           <div className="wrapper">
-            <p class="statement">I found AI’s reaction to errors appropriate</p>
+            <p class="statement">In my opinion, the AI assistant provided a satisfactory response to the error.</p>
             <div>
               strongly disagree<input type="radio" Value="1" name="q6" onChange={this.onChange.bind(this)}/>
               <input type="radio" Value="2" name="q6" onChange={this.onChange.bind(this)}/>
@@ -162,7 +184,7 @@ class Questionaire extends React.Component {
           </div>
 
           <div className="wrapper">
-            <p class="statement">AI explained itself reasonably after the error occurred</p>
+            <p class="statement">AI assistant acknowledged the mistake as its own.</p>
             <div>
               strongly disagree<input type="radio" Value="1" name="q7" onChange={this.onChange.bind(this)}/>
               <input type="radio" Value="2" name="q7" onChange={this.onChange.bind(this)}/>
@@ -173,13 +195,13 @@ class Questionaire extends React.Component {
             </div>
           </div>
           <div className="wrapper">
-            <p class="statement">AI assistant was able to understand its mistake and correct it effectively</p>
+            <p class="statement">AI assistant shifted blame from itself.</p>
             <div>
-              strongly disagree<input type="radio" Value="1" name="q7" onChange={this.onChange.bind(this)}/>
-              <input type="radio" Value="2" name="q7" onChange={this.onChange.bind(this)}/>
-              <input type="radio" Value="3" name="q7" onChange={this.onChange.bind(this)}/>
-              <input type="radio" Value="4" name="q7" onChange={this.onChange.bind(this)}/>
-              <input type="radio" Value="5" name="q7" onChange={this.onChange.bind(this)}/> strongly agree
+              strongly disagree<input type="radio" Value="1" name="q8" onChange={this.onChange.bind(this)}/>
+              <input type="radio" Value="2" name="q8" onChange={this.onChange.bind(this)}/>
+              <input type="radio" Value="3" name="q8" onChange={this.onChange.bind(this)}/>
+              <input type="radio" Value="4" name="q8" onChange={this.onChange.bind(this)}/>
+              <input type="radio" Value="5" name="q8" onChange={this.onChange.bind(this)}/> strongly agree
               <br />            
             </div>
           </div>
@@ -189,18 +211,8 @@ class Questionaire extends React.Component {
         <div>
           <h2>Please rate your impression of the agent with the following scales:</h2>
 
-          <div className="quest-wrapper">
-            <p className="item1">Unconscious</p>
-            <form className="item2">
-              <label><input type="radio" Value="1" name="q8" onChange={this.onChange.bind(this)}/></label>
-              <label><input type="radio" Value="2" name="q8" onChange={this.onChange.bind(this)}/></label>
-              <label><input type="radio" Value="3" name="q8" onChange={this.onChange.bind(this)}/></label>
-              <label><input type="radio" Value="4" name="q8" onChange={this.onChange.bind(this)}/></label>
-              <label><input type="radio" Value="5" name="q8" onChange={this.onChange.bind(this)}/></label>
-            </form>
-            <p className="item3">Conscious</p>
-            <br />  
-            <p className="item1">Apathetic</p>
+          <div className="quest-wrapper"> 
+            <p className="item1">Dislike</p>
             <form className="item2">
               <label><input type="radio" Value="1" name="q9" onChange={this.onChange.bind(this)}/></label>
               <label><input type="radio" Value="2" name="q9" onChange={this.onChange.bind(this)}/></label>
@@ -208,7 +220,7 @@ class Questionaire extends React.Component {
               <label><input type="radio" Value="4" name="q9" onChange={this.onChange.bind(this)}/></label>
               <label><input type="radio" Value="5" name="q9" onChange={this.onChange.bind(this)}/></label>
             </form>
-            <p className="item3">Responsive</p>
+            <p className="item3">Like</p>
             <br />            
             <p className="item1">Unfriendly</p>
             <form className="item2">
@@ -228,7 +240,7 @@ class Questionaire extends React.Component {
               <label><input type="radio" Value="4" name="q11" onChange={this.onChange.bind(this)}/></label>
               <label><input type="radio" Value="5" name="q11" onChange={this.onChange.bind(this)}/></label>
             </form>
-            <p className="item3">Knowledgeable</p>
+            <p className="item3">Competent</p>
             <br />            
             <p className="item1">Irresponsible</p>
             <form className="item2">
@@ -239,6 +251,36 @@ class Questionaire extends React.Component {
               <label><input type="radio" Value="5" name="q12" onChange={this.onChange.bind(this)}/></label>
             </form>
             <p className="item3">Responsible</p>
+            <br/>
+            <p className="item1">Awful</p>
+            <form className="item2">
+              <label><input type="radio" Value="1" name="q13" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="2" name="q13" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="3" name="q13" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="4" name="q13" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="5" name="q13" onChange={this.onChange.bind(this)}/></label>
+            </form>
+            <p className="item3">Nice</p>
+            <br /> 
+            <p className="item1">Ignorant</p>
+            <form className="item2">
+              <label><input type="radio" Value="1" name="q14" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="2" name="q14" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="3" name="q14" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="4" name="q14" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="5" name="q14" onChange={this.onChange.bind(this)}/></label>
+            </form>
+            <p className="item3">Knowledgeable</p>
+            <br /> 
+            <p className="item1">Foolish</p>
+            <form className="item2">
+              <label><input type="radio" Value="1" name="q15" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="2" name="q15" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="3" name="q15" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="4" name="q15" onChange={this.onChange.bind(this)}/></label>
+              <label><input type="radio" Value="5" name="q15" onChange={this.onChange.bind(this)}/></label>
+            </form>
+            <p className="item3">Intelligent</p> 
           </div>
       </div>
 
