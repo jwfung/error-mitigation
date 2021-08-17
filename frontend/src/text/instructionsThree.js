62d026867ce438a4175bac935f0d1d2860c94788
jwfung <59128478+jwfung@users.noreply.github.com>
2021-08-16 23:10:58 -0400

    typo fixed

--- frontend/src/text/instructionsThree.js
+++ frontend/src/text/instructionsThree.js
@@ -20,7 +20,7 @@ const instructionsThree = [
     mid: "Click the arrow to see what you need, and then proceed to the session."
   },
   {
-    top: <p>"Hi! Can you help me finish packing for school? I still need a <strong>backkpack</strong> to store all my things. My mom said I need new <strong>glasses</strong> to help me see the board better. I also need some <strong>#2 pencils</strong> and <strong>a notebook</strong> to take notes in, and <strong>glue</strong> for projects. I think that's all that I'm missing, thank you!"</p>
+    top: <p>"Hi! Can you help me finish packing for school? I still need a <strong>backpack</strong> to store all my things. My mom said I need new <strong>glasses</strong> to help me see the board better. I also need some <strong>#2 pencils</strong> and <strong>a notebook</strong> to take notes in, and <strong>glue</strong> for projects. I think that's all that I'm missing, thank you!"</p>
   }
 ];
 
